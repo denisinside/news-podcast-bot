71f17b58ff4d9bdaf65accfa5275b6bf34c8b639
denisinside <d.shvachka@ukma.edu.ua>
2025-10-07 17:28:32 +0300

    Fix scenes issues

--- src/infrastructure/repositories/ISubscriptionRepository.ts
+++ src/infrastructure/repositories/ISubscriptionRepository.ts
@@ -5,7 +5,7 @@ export interface ISubscriptionRepository {
     create(userId: string, topicId: Types.ObjectId): Promise<ISubscription>;
     delete(userId: string, topicId: Types.ObjectId): Promise<boolean>;
     exists(userId: string, topicId: Types.ObjectId): Promise<boolean>;
-    findByUserId(id: string): Promise<ISubscription | null>;
+    findByUserId(id: string): Promise<ISubscription[]>;
     findAll(): Promise<ISubscription[]>;
     update(id: string, updateData: Partial<ISubscription>): Promise<ISubscription | null>;
 }
